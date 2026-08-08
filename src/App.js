b857f7738d2854b825e9d25f17d4efa7d46043e0
diego bucci <forest73@libero.it>
2026-08-08 16:26:57 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function ScrollToTop() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/cortona">
       <LanguageProvider>
         <ScrollToTop />
         <Seo />
